cc195c456148df89ce625871cbb83bdec79f4594
buddipn <33357286+buddipn@users.noreply.github.com>
2017-11-20 22:05:06 -0500

    Update Rectangulator.java

--- src/Rectangulator.java
+++ src/Rectangulator.java
@@ -3,9 +3,9 @@ public class Rectangulator{
     int length = Interger.parseInt(args[0]);
     int width = Interger.parseInt(args[1]);
 
-    Reactangle myRectangle = new Reactangle(length, width);
+    Rectangle myRectangle = new Rectangle(length, width);
 
-    String output = String.format("***** Your Reactangle *****\n\nLength: %d\nWidth: %d\nArea: %d\nPerimeter: %d\n\n", myRectangle.length, myRectangle.width, myRectangle.getArea(), myRectangle.getPerimeter());
+    String output = String.format("***** Your Rectangle *****\n\nLength: %d\nWidth: %d\nArea: %d\nPerimeter: %d\n\n", myRectangle.length, myRectangle.width, myRectangle.getArea(), myRectangle.getPerimeter());
 
 
     System.out.println(output);
